f8f5a58f4ddc3ae96886fc17d4bb6a26154c7b0f
Aisha Awudu <awuduaisha@gmail.com>
2023-01-23 14:26:04 -0800

    updated _printf.c file, tried fixing errors

--- _printf.c
+++ _printf.c
@@ -2,63 +2,77 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * _printf -similar to inbuilt printf
- * @format: first arg
- * @...: variable args
- * Return: char count
- */
-
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*find_function(const char *format))(va_list)
+{
+	unsigned int p = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	while (find_f[p].sc)
+	{
+		if (find_f[p].sc[0] == (*format))
+			return (find_f[p].f);
+		p++;
+	}
+	return (NULL);
+}
+/**
+  * _printf - function gives output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output
+  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0;
-	char buffer[100];
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int p = 0, printcount = 0;
 
-	va_start(args, format);
-	if (format)
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[p])
 	{
-		while (format[i] != '\0')
+		while (format[p] != '%' && format[p])
 		{
-			if (format[i] != '%')
-			{
-				write_char(format[i]);
-				i++;
-			}
-			else
-			{
-				switch (format[i + 1])
-				{
-					case 'c':
-						write_char(va_arg(args, int));
-						break;
-					case 's':
-						write_string(va_arg(args, char *));
-						break;
-					case 'i':
-					case 'd':
-						gcvt(va_arg(args, int), -1, buffer);
-						write_string(buffer);
-						break;
-					case 'f':
-						/**gcvt (float value, int ndigits, char * buf);*/
-						gcvt(va_arg(args, double), 6, buffer);
-						write_string(buffer);
-						break;
-					case '%':
-						write_char(format[i + 1]);
-						break;
-					default:
-						write_char(format[i]);
-						write_char(format[i + 1]);
-						break;
-				}
-				i += 2;
-			}
+			_putchar(format[p]);
+			printcount++;
+			p++;
 		}
+		if (format[p] == '\0')
+			return (printcount);
+		f = find_function(&format[p + 1]);
+		if (f != NULL)
+		{
+			printcount += f(ap);
+			p += 2;
+			continue;
+		}
+		if (!format[p + 1])
+			return (-1);
+		_putchar(format[p]);
+		printcount++;
+		if (format[p + 1] == '%')
+			p += 2;
+		else
+			p++;
 	}
-	va_end(args);
-	return (i);
+	va_end(ap);
+	return (printcount);
 }
-
